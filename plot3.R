d44ae9e761544a2d96c101e6026f41847048d5a7
Satyapriya Krishna <krishna.satyapriya123@gmail.com>
2015-12-12 18:54:12 +0530

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -1,4 +1,4 @@
-dataFile <- "household_power_consumption.txt"
+dataFile <- "./data/household_power_consumption.txt"
 data <- read.table(dataFile, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
 subSetData <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
 
